490fad4667724fc5c8a6d0e8bc855a88f71a8531
Brian Todoroff <brian.github@todoroff.com>
2012-08-19 19:24:42 -0700

    Merge in the main branch changes

--- e16Controls/RegisterView.xaml.cs
+++ e16Controls/RegisterView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Copyright 2012 Brian Todoroff
+ * Encephal16 by Brian Todoroff is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License
+ * Based on a work at https://github.com/btodoroff/Encephal16.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
